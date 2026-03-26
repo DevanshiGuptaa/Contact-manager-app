ab6d65922857f6968890da5b33d160f341c684d8
DevanshiGuptaa <devanshigupta953@gmail.com>
2026-03-26 18:10:39 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -4,27 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('Backend is running...');
-});
-
-// Routes
 app.use('/api/contacts', require('./routes/contactRoutes'));
 
-// Port
-const PORT = process.env.PORT || 5000;
-
-// MongoDB connection + server start
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}`)
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
     );
   })
   .catch(err => console.log(err));
